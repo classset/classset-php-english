d2290b0b4687f6a575ea012c64cd5e8b65e688b6
Gabriel Nicolás González Ferreira <gabrielinuz@gmail.com>
2015-06-23 22:11:04 -0300

    database move

--- back-end/application/AAA_Module/actions/A_Authenticate.php
+++ back-end/application/AAA_Module/actions/A_Authenticate.php
@@ -26,42 +26,42 @@ class A_Authenticate implements IAction
 {
     public function execute()
     {
-		//SESSION
-		$session = SessionFactory::create();
+        //SESSION
+        $session = SessionFactory::create();
 
-		//PARAMETERS
-		$params = RequestParametersFactory::create();
-		$session = SessionFactory::create();
+        //PARAMETERS
+        $params = RequestParametersFactory::create();
+        $session = SessionFactory::create();
 
-		if( !$session->get("authenticated") and $params->get('public_key') == $session->get('randLogin') )
-		{
-			//PARAMETERS:
-			$params = RequestParametersFactory::create();
-			$username = $params->get('user-name');
-			$userpassword = $params->get('user-password');
-			$filter = FilterFactory::create();
-			$filteredUsername = $filter->filters($username);
+        if( !$session->get("authenticated") and $params->get('public_key') == $session->get('randLogin') )
+        {
+            //PARAMETERS:
+            $params = RequestParametersFactory::create();
+            $username = $params->get('user-name');
+            $userpassword = $params->get('user-password');
+            $filter = FilterFactory::create();
+            $filteredUsername = $filter->filters($username);
 
-			//DATAHANDLER
-			$datahandler = DatahandlerFactory::create('D_ReadUserByName');
-			$datahandler->setInData($filteredUsername);
-			$existingUser = $datahandler->getOutData();
+            //DATAHANDLER
+            $datahandler = DatahandlerFactory::create('D_ReadUserByName');
+            $datahandler->setInData($filteredUsername);
+            $existingUser = $datahandler->getOutData();    
 
-			//PASSWORD VERIFY
-			$isAuthenticate = (password_verify($userpassword, $existingUser['password']));
+            //ENCRYPTOR
+            $isAuthenticate = (password_verify($userpassword, $existingUser['password']));
 
-			if( $isAuthenticate )
-			{
-				//SET SESSION DATA
-				$session->set('session-user-name', $existingUser['name']);
-				$session->set('session-user-id', $existingUser['id']);
-				$session->set("authenticated", true);
-			}
-			else
-			{
-				$session->set("authenticated", false);
-			}
-		}
-	}
+            if( $isAuthenticate )
+            {
+                //SET SESSION DATA
+                $session->set('session-user-name', $existingUser['name']);
+                $session->set('session-user-id', $existingUser['id']);
+                $session->set("authenticated", true);
+            }
+            else
+            {
+                $session->set("authenticated", false);
+            }            
+        }
+    }
 }
 ?>
