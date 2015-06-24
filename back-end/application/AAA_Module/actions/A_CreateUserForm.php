94366f215d8f3609bfbcbf733aeef3125407f853
Gabriel Nicolás González Ferreira <gabrielinuz@gmail.com>
2015-06-23 23:52:59 -0300

    comestic

--- back-end/application/AAA_Module/actions/A_CreateUserForm.php
+++ back-end/application/AAA_Module/actions/A_CreateUserForm.php
@@ -24,11 +24,10 @@
 
 class A_CreateUserForm implements IAction
 {
-	public function execute()
-	{
-    	$view = ViewFactory::create('V_CreateUserForm');
-    	$view->display();
+    public function execute()
+    {
+        $view = ViewFactory::create('V_CreateUserForm');
+        $view->display();
     }
 }
-
 ?>
